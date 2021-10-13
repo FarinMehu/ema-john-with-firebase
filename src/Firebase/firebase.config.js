f8e5e3b903dfc084778cd888c96f6c1e370f5782
Mehjabin Khan Farin <80196064+FarinMehu@users.noreply.github.com>
2021-10-13 11:16:08 +0600

    commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAx-Q4xtUTN3LhxbLzc6Nk8SFUmX1x3ktE",
+    authDomain: "simple-ema-john-652fa.firebaseapp.com",
+    projectId: "simple-ema-john-652fa",
+    storageBucket: "simple-ema-john-652fa.appspot.com",
+    messagingSenderId: "308997027453",
+    appId: "1:308997027453:web:f81c4378ff94ea691f6314"
+};
+
+export default firebaseConfig;
